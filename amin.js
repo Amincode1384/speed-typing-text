37c2a6f4d77b73ebfbffde342abfe15e0ce9040e
mohammad amin <izadi.mohammad.1384@gmail.com>
2025-08-30 12:19:22 +0330

    write script

--- amin.js
+++ amin.js
@@ -1,17 +1,27 @@
-const theTimer=document.querySelector(".timer")
+const theTimer=document.querySelector(".Timer");
+let currentTime;
 
 
+var timer=[0,0,0,0];
 
-var time=[0,0,0,0];
+function leadingzero(time){
+if(time<=9){
+    time="0"+time;
+}else{
+    time=time;
+}
 
-
+}
 
 
 function runTimer(){
-let currentTime=timer[0]+":"+timer[1]+":"+timer[2];
+ currentTime=timer[0]+":"+timer[1]+":"+timer[2];
 theTimer.innerHTML= currentTime;
 timer[3]++;
-timer[0]=timer[3]/100/60;
+timer[0] = Math.floor(timer[3]/100/60); // دقیقه
+timer[1] = Math.floor(timer[3]/100) % 60; // ثانیه
+timer[2] = timer[3] % 100; // صدم ثانیه
+
 
 }
 
